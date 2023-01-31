db03d84b06f1b6a1c5c6013560baeee4b5ca3336
LeonHu <hulj@playable.cn>
2023-01-31 15:15:44 +0800

    [Change] annotation from android.support to androidx

--- android/src/main/java/com/dylanvann/fastimage/FastImageViewModule.java
+++ android/src/main/java/com/dylanvann/fastimage/FastImageViewModule.java
@@ -2,7 +2,7 @@ package com.dylanvann.fastimage;
 
 import android.app.Activity;
 
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.DataSource;
@@ -16,7 +16,6 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
-import com.facebook.react.views.imagehelper.ImageSource;
 
 import java.io.File;
 
